aa2e6db1e9b4a6ba8602bbf744938bcb8428b3f3
FengJunJian <446187960@qq.com>
2020-08-21 20:17:30 +0800

    test 0.1

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# dataset_metric
-Metrics of dataset with voc format
+# dataset metric
+Metrics of dataset with **VOC** format
 
 1.Go to  ./coco/PythonAPI
 
@@ -8,3 +8,23 @@ Metrics of dataset with voc format
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Run `python setup.py build_ext install`
 
 2.Go to ./utils and run `python setup.py build_ext --inplace`
+
+3.Run main.py
+
+-----------------------------------------------------------------------------------------
+
+​	Please modify the following variables:
+
+​	**In  main.py**
+
+​	*dataset_path*: VOC dataset path.
+
+​	*split_name*: Which mainset to evaluate.
+
+​	**In  semi_pascal_voc.py**
+
+​	class_name: modify as the class_names in your dataset.
+
+-----------------------------
+
+​	
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from imdb import imdb as imdb2
 from semi_factory import get_imdb
 import roidb as rdl_roidb
 import os
+import pickle
 
 abs_path='.'
 
@@ -53,23 +54,54 @@ def combined_roidb(imdb_names,image_set='train',dataset_path=None):
         imdb = get_imdb(imdb_names,image_set,dataset_path)
     return imdb, roidb
 
+def combined_roidb_un(imdb_names,image_set='train',dataset_path=None):
+    """
+        Combine multiple roidbs
+        """
+    def get_roidb(imdb_name):
+        imdb = get_imdb(imdb_name, image_set,dataset_path)  # 返回voc数据集
+        print('Loaded dataset `{:s}` for training'.format(imdb.name))
+        imdb.set_proposal_method("unlabel")  # 设置获取注释文件方式
+        print('Set proposal method: {:s}'.format("unlabel"))
+        roidb = get_testing_roidb(imdb,True)
+        return roidb
+
+    roidbs = [get_roidb(s) for s in imdb_names.split('+')]  # search database 可多个数据集以‘+’区分
+    roidb = roidbs[0]
+    if len(roidbs) > 1:
+        for r in roidbs[1:]:
+            roidb.extend(r)
+        tmp = get_imdb(imdb_names.split('+')[1], image_set,dataset_path)
+        imdb = imdb2(imdb_names, tmp.classes)
+    else:
+        imdb = get_imdb(imdb_names, image_set,dataset_path)
+    return imdb, roidb
+
 def main():
-    #model_name = cfg.FLAGS.network_semi
-    #self.num = cfg.FLAGS.test_iter  # 10000
     dataset_path='E:/fjj/SeaShips_SMD/'
+    split_name='test650'
     model_name='semi_faster'
     log_path='test'
-    model_path = 'E:/fjj/semi_ship/vgg16_semi/label0_unlabel0/vgg16_semi_faster_rcnn_iter_192000.ckpt'
-
-    imdb, roidb = combined_roidb("shipdataset", 'test650',dataset_path)  # 测试数据集test1300,test650
-    # self.imdb, self.roidb=combined_roidb_un("unlabel_dataset",'unlabel_train',cfg.FLAGS2['semi_unlabeled_dir'])
+    imdb, roidb = combined_roidb("shipdataset", split_name,dataset_path)  # 测试数据集test1300,test650
+    # unimdb, unroidb=combined_roidb_un("unlabel_dataset",'unlabel_train','semi_unlabeled_dir')
     output_dir = get_output_dir(imdb.name, log_path, model_name)  # 创建文件'wideresnet'
-    #trained_model = cfg.FLAGS.label_sets + '_' + cfg.FLAGS.unlabel_sets
-    tfmodel='E:/fjj/semi_ship/vgg16_semi/label0_unlabel0/vgg16_semi_faster_rcnn_iter_192000.ckpt'
-    tfmeta='E:/fjj/semi_ship/vgg16_semi/label0_unlabel0/vgg16_semi_faster_rcnn_iter_192000.ckpt.meta'
-    sess=tf.Session()
-    saver = tf.train.import_meta_graph(tfmeta)
-    saver.restore(sess, tfmodel)
+
+    all_boxes = [[[] for _ in range(imdb.num_images)]
+                 for _ in range(imdb.num_classes)]
+    '''
+    all_boxes:shape (num_images,num_classes)
+    each boxes is stored as np.array([xmin,ymin,xmax,ymax,score])
+    that is the the second image and 1-index class  
+    all_boxes[1][2]=boxes
+    boxes:shape (batch,5) :np.array([[xmin_1,ymin_1,xmax_1,ymax_1,score_1],......,[xmin_n,ymin_n,xmax_n,ymax_n,score_n]])
+    '''
+
+    pkl_file='detections.pkl'
+    with open(pkl_file, 'rb') as f:
+        all_boxes = pickle.load(f)
+    print('Evaluating detections')
+    aps = imdb.evaluate_detections(all_boxes, output_dir)
+    print(aps)
 
 
 
