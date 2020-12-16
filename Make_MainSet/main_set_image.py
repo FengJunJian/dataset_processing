479a63b131e867429b9befeed18d423b1bfd9258
FengJunJian <446187960@qq.com>
2020-12-16 10:58:48 +0800

    make the mainset according the name of images

---
+++ Make_MainSet/main_set_image.py
@@ -0,0 +1,15 @@
+'''
+make the mainset according the name of images
+'''
+import os
+import glob
+datasetPath='E:/fjj/Dataset100'
+JPEGImagesPah=os.path.join(datasetPath,'JPEGImages')
+MainPath=os.path.join(datasetPath,'ImageSets/Main')
+files=os.listdir(JPEGImagesPah)
+basenames=[os.path.splitext(file)[0]+'\n' for file in files]
+with open(os.path.join(MainPath,'test.txt'),'w') as f:
+    f.writelines(basenames)
+#files=glob.glob(os.path.join(JPEGImagesPah,'*.jpg'))
+
+
