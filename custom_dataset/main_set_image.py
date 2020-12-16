a6e0667f3241de4774268d828760f062a49628f0
FengJunJian <446187960@qq.com>
2020-12-16 11:16:45 +0800

    updates

--- custom_dataset/main_set_image.py
+++ custom_dataset/main_set_image.py
@@ -10,6 +10,9 @@ files=os.listdir(JPEGImagesPah)
 basenames=[os.path.splitext(file)[0]+'\n' for file in files]
 with open(os.path.join(MainPath,'test.txt'),'w') as f:
     f.writelines(basenames)
+
+targetAnnoPath=os.path.join(datasetPath,'Annotations')
+sourceAnnoPath=os.path.join()
 #files=glob.glob(os.path.join(JPEGImagesPah,'*.jpg'))
 
 
