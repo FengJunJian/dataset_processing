f865b971c7ff091877103f568162f701d51bb969
FengJunJian <446187960@qq.com>
2021-06-29 21:38:47 +0800

    tool function

--- visual_function.py
+++ visual_function.py
@@ -44,11 +44,10 @@ def write_detection_batch(im, class_inds, dets,CLASSES,colors):
         bbox = dets[i, :4].astype(np.int32)
         # score = dets[i, -1]
         rectangle_tmp = im.copy()
-        cv2.rectangle(rectangle_tmp, (bbox[0], bbox[1]), (bbox[2], bbox[3]), colors[class_inds[i]], 3)#10
         #str1=CLASSES[class_inds[i]]
         string = '%s' % (CLASSES[class_inds[i]])
         fontFace = cv2.FONT_HERSHEY_COMPLEX#cv2.FONT_HERSHEY_COMPLEX
-        fontScale=1#2
+        fontScale=1.#2
         thiness=1#2
 
         text_size,baseline = cv2.getTextSize(string, fontFace, fontScale, thiness)
@@ -59,7 +58,8 @@ def write_detection_batch(im, class_inds, dets,CLASSES,colors):
                       (text_origin[0] + text_size[0], text_origin[1] - text_size[1] - 1), colors[class_inds[i]],
                       cv2.FILLED)
 
-        cv2.addWeighted(im,0.7,rectangle_tmp,0.3,0,im)
+        cv2.addWeighted(im,0.6,rectangle_tmp,0.4,0,im)
+        im=cv2.rectangle(im, (bbox[0], bbox[1]), (bbox[2], bbox[3]), colors[class_inds[i]], 3)  # 10
         cv2.putText(im, string, text_origin,
                     fontFace, fontScale, (0, 0, 0), thiness, lineType=-1, )
     return im
