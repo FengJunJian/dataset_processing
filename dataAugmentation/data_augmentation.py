be02ac388ab0ddd0706541a8319860e9664c60d4
FengJunJian <446187960@qq.com>
2021-10-28 16:44:06 +0800

    updates

--- dataAugmentation/data_augmentation.py
+++ dataAugmentation/data_augmentation.py
@@ -5,65 +5,78 @@ import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 
-root='E:/fjj/SeaShips_SMD/JPEGImages'
-imgname='MVI_1582_VIS_00253.jpg'#'MVI_1644_VIS_00203.jpg'
-imagepath=os.path.join(root,imgname)
+def aug1():
+    root='E:/fjj/SeaShips_SMD/JPEGImages'
+    imgname='MVI_1582_VIS_00253.jpg'#'MVI_1644_VIS_00203.jpg'
+    imagepath=os.path.join(root,imgname)
 
-img=Image.open(imagepath)
-imgnp=np.array(img)
-plt.figure(1)
-plt.imshow(imgnp)
-#plt.show(1)
-transform=transforms.Compose(
-                    [
-                        #transforms
+    img=Image.open(imagepath)
+    imgnp=np.array(img)
+    plt.figure(1)
+    plt.imshow(imgnp)
+    #plt.show(1)
+    transform=transforms.Compose(
+                        [
+                            #transforms
 
-                        #transforms.RandomCrop((1080,1920), padding=4),
-                        # transforms.RandomCrop((1080,1080), padding=4),
-                        transforms.RandomResizedCrop((1080,1920), (0.8, 1.0),(3.0/4,1920.0/1080.0)),
+                            #transforms.RandomCrop((1080,1920), padding=4),
+                            # transforms.RandomCrop((1080,1080), padding=4),
+                            transforms.RandomResizedCrop((1080,1920), (0.8, 1.0),(3.0/4,1920.0/1080.0)),
 
-                        transforms.RandomHorizontalFlip(),
-                        transforms.RandomApply(
-                            [transforms.ColorJitter(0.3, 0.3, 0.15, 0.1)], p=0.5
-                        ),
-                        transforms.RandomGrayscale(p=0.2),
-                        #transforms.Resize((1080,1920))
-                        #transforms.ToTensor(),
-                        #transforms.Normalize(mean, std),
-                    ]
-                )
-# transform=transforms.Compose(
-#                     [
-#                         #transforms
-#                         transforms.RandomChoice(
-#                             [
-#                                 transforms.RandomCrop((1080,1920), padding=4),
-#                                 transforms.RandomResizedCrop((1080,1920), (0.8, 1.0)),
-#                             ]
-#                         ),
-#                         transforms.RandomHorizontalFlip(),
-#                         transforms.RandomApply(
-#                             [transforms.ColorJitter(0.3, 0.3, 0.15, 0.1)], p=0.5
-#                         ),
-#                         transforms.RandomGrayscale(p=0.15),
-#                         #transforms.ToTensor(),
-#                         #transforms.Normalize(mean, std),
-#                     ]
-#                 )
+                            transforms.RandomHorizontalFlip(),
+                            transforms.RandomApply(
+                                [transforms.ColorJitter(0.3, 0.3, 0.15, 0.1)], p=0.5
+                            ),
+                            transforms.RandomGrayscale(p=0.2),
+                            #transforms.Resize((1080,1920))
+                            #transforms.ToTensor(),
+                            #transforms.Normalize(mean, std),
+                        ]
+                    )
+    # transform=transforms.Compose(
+    #                     [
+    #                         #transforms
+    #                         transforms.RandomChoice(
+    #                             [
+    #                                 transforms.RandomCrop((1080,1920), padding=4),
+    #                                 transforms.RandomResizedCrop((1080,1920), (0.8, 1.0)),
+    #                             ]
+    #                         ),
+    #                         transforms.RandomHorizontalFlip(),
+    #                         transforms.RandomApply(
+    #                             [transforms.ColorJitter(0.3, 0.3, 0.15, 0.1)], p=0.5
+    #                         ),
+    #                         transforms.RandomGrayscale(p=0.15),
+    #                         #transforms.ToTensor(),
+    #                         #transforms.Normalize(mean, std),
+    #                     ]
+    #                 )
 
-savetmp='../data_processing_cache/dataAug'
+    savetmp='../data_processing_cache/dataAug'
 
-if not os.path.exists(savetmp):
-    os.mkdir(savetmp)
+    if not os.path.exists(savetmp):
+        os.mkdir(savetmp)
 
 
-a=transform(img)
-anp=np.array(a)
-plt.figure(2)
-plt.imshow(anp)
-a.save(os.path.join(savetmp,'a'+imgname))
-a=transform(img)
-anp=np.array(a)
-plt.figure(3)
-plt.imshow(anp)
-a.save(os.path.join(savetmp,'b'+imgname))
+    a=transform(img)
+    anp=np.array(a)
+    plt.figure(2)
+    plt.imshow(anp)
+    a.save(os.path.join(savetmp,'a'+imgname))
+    a=transform(img)
+    anp=np.array(a)
+    plt.figure(3)
+    plt.imshow(anp)
+    a.save(os.path.join(savetmp,'b'+imgname))
+
+def postprocess():
+    root = '../../data_processing_cache\dataAug'
+    files=['aMVI_1582_VIS_00253.jpg','aMVI_1644_VIS_00203.jpg','MVI_1582_VIS_00253.jpg','MVI_1644_VIS_00203.jpg']
+    for file in files:
+        filedir=os.path.join(root,file)
+        img=cv2.imread(filedir)
+        dst=cv2.resize(img,dsize=None,fx=0.2,fy=0.2)
+        cv2.imshow('a',dst)
+        cv2.waitKey(1)
+        cv2.imwrite(os.path.join(root,"resize"+file),dst)
+postprocess()
