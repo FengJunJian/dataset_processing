0f6c19a5d04a0be6ff0ec77421d9eb08f719bbb5
FengJunJian <446187960@qq.com>
2021-11-18 10:35:34 +0800

    file tools

--- FileTools.py
+++ FileTools.py
@@ -1,10 +1,27 @@
 import os
 import shutil
 
-path='E:/fjj/MarineShips2/JPEGImages'
-files=os.listdir(path)
-for file in files:
-    oldfilename=os.path.join(path,file)
-    newfilename=os.path.join(path,file.replace(' ','_'))
-#newfile=file.replace(' ','_')
-    os.rename(oldfilename,newfilename)
+def renameBatch():
+    '''
+    rename
+    '''
+    path='E:/fjj/MarineShips2/JPEGImages'
+    files=os.listdir(path)
+    for file in files:
+        oldfilename=os.path.join(path,file)
+        newfilename=os.path.join(path,file.replace(' ','_'))
+    #newfile=file.replace(' ','_')
+        os.rename(oldfilename,newfilename)
+
+
+
+
+def removeBatch():
+    '''
+    批量删除文件
+
+    '''
+    root='E:/FE/'
+    files=os.listdir(root)
+    for i in range(0,len(files),4):
+        os.remove(os.path.join(root,files[i]))
