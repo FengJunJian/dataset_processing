94f05f3a9e29ecf812be9ef538bca211b8d0e066
FengJunJian <446187960@qq.com>
2021-12-31 10:47:23 +0800

    colors generator

--- Visualization/generateColor.py
+++ Visualization/generateColor.py
@@ -1,6 +1,6 @@
 import random
 import colorsys
-def ncolors(num):
+def ncolorsRandom(num):
     rgb_colors = []
     if num < 1:
         return rgb_colors
@@ -21,3 +21,13 @@ def ncolors(num):
         rgb_colors.append([r, g, b])
 
     return rgb_colors
+
+def ncolors(num_color):
+    hsv_tuples = [(x / num_color, 1., 1.)
+                  for x in range(num_color)]
+    colors = list(map(lambda x: colorsys.hsv_to_rgb(*x), hsv_tuples))
+    colors = list(
+        map(lambda x: (int(x[0] * 255), int(x[1] * 255), int(x[2] * 255)),
+            colors))
+    colors = [c[::-1] for c in colors]
+    return colors
