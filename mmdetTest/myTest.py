a5a6a49c4fc1ade5956bbe1e36d683176e606af6
FengJunJian <446187960@qq.com>
2022-03-04 16:46:24 +0800

    mmdetection安装验证

---
+++ mmdetTest/myTest.py
@@ -0,0 +1,21 @@
+from mmdet.apis import init_detector, inference_detector,show_result_pyplot
+import mmcv
+import os
+
+absP='D:/mmdetection'#模型路径
+config_file = os.path.join(absP,'configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py')
+# 从 model zoo 下载 checkpoint 并放在 `checkpoints/` 文件下
+# 网址为: http://download.openmmlab.com/mmdetection/v2.0/faster_rcnn/faster_rcnn_r50_fpn_1x_coco/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth
+checkpoint_file = '../../data_processing_cache/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'
+device = 'cuda:0'
+# 初始化检测器
+model = init_detector(config_file, checkpoint_file, device=device)
+# 推理演示图像
+img=os.path.join(absP,'demo/demo.jpg')
+result=inference_detector(model, img)
+outImg=model.show_result(
+img,
+result)
+#outImg1=show_result_pyplot(model,img,result)
+mmcv.imshow(outImg,'a')
+print(result)
