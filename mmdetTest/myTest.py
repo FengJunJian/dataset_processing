9ed3e2965aad3ff1cb1f819373d1a145404f1596
FengJunJian <446187960@qq.com>
2022-04-27 09:33:22 +0800

    updates test for other data

--- mmdetTest/myTest.py
+++ mmdetTest/myTest.py
@@ -7,15 +7,16 @@ config_file = os.path.join(absP,'configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco
 # 从 model zoo 下载 checkpoint 并放在 `checkpoints/` 文件下
 # 网址为: http://download.openmmlab.com/mmdetection/v2.0/faster_rcnn/faster_rcnn_r50_fpn_1x_coco/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth
 checkpoint_file = '../../data_processing_cache/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'
-device = 'cuda:0'
+device = 'cpu'
 # 初始化检测器
 model = init_detector(config_file, checkpoint_file, device=device)
 # 推理演示图像
-img=os.path.join(absP,'demo/demo.jpg')
+img=os.path.join('012.jpg')#'absP,demo/demo.jpg'
 result=inference_detector(model, img)
 outImg=model.show_result(
 img,
 result)
 #outImg1=show_result_pyplot(model,img,result)
-mmcv.imshow(outImg,'a')
+#mmcv.imshow(outImg,'a')
+mmcv.imwrite(outImg,'aa.jpg')
 print(result)
