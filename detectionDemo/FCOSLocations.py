3aa7e8814521f2dd6b7be965605b7f91a5e2581f
FengJunJian <446187960@qq.com>
2022-05-26 17:04:36 +0800

    add for FCOS

---
+++ detectionDemo/FCOSLocations.py
@@ -0,0 +1,18 @@
+import torch
+def compute_locations_per_level(h, w, stride, device):
+    shifts_x = torch.arange(
+        0, w * stride, step=stride,
+        dtype=torch.float32, device=device
+    )
+    shifts_y = torch.arange(
+        0, h * stride, step=stride,
+        dtype=torch.float32, device=device
+    )
+    shift_y, shift_x = torch.meshgrid(shifts_y, shifts_x)
+    shift_x = shift_x.reshape(-1)
+    shift_y = shift_y.reshape(-1)
+    locations = torch.stack((shift_x, shift_y), dim=1) + stride // 2
+    return locations
+
+if __name__=="__main__":
+    locations=compute_locations_per_level(5,6,1,"cpu")#位置偏移矩阵
