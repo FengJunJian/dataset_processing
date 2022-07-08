620c8cdf90c5968e43cbc13c38a3ee5beb838c6e
FengJunJian <446187960@qq.com>
2022-07-08 09:21:18 +0800

    updates

--- detectionDemo/thopDemo.py
+++ detectionDemo/thopDemo.py
@@ -2,8 +2,11 @@ import torchvision
 import torch
 import thop
 
+
 x = torch.randn(1, 3, 224, 224)
 modelt = torchvision.models.detection.fasterrcnn_mobilenet_v3_large_320_fpn()
 flops1,params1=thop.profile(modelt, (x,))
 print("%s ------- params: %.2fMB ------- flops: %.2fG" % (
     modelt._get_name(), params1 / (1000 ** 2), flops1 / (1000 ** 3)))  #
+
+
